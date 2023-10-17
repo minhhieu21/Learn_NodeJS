d85cd2409229eab4932fb900ac465b41ae299fd6
minhhieu2104 <hieutmpd05172@fpt.edu.vn>
2023-10-17 23:58:52 +0700

    edit project

--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
 const express = require('express') //import express
 const app = express() // tạo express application
-const port = 3000 // init port
 const path = require('path')
+
+// config dotenv
+require('dotenv').config()
+
+const port = process.env.PORT || 8081 // init port
+const hostname = process.env.HOST_NAME // init port
+
+
+// config static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // config templat engine
 app.set('views', path.join(__dirname, 'views'));
@@ -10,15 +19,15 @@ app.set('view engine', 'ejs')
 //khai báo routes
 //req (request), res(response) là 2 object trong môi trường Node.js
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World & nodemon')
 })
 
 app.get('/test', (req, res) => {
-    res.render('sample.ejs')
+    res.render('index.ejs')
 })
 
 //run server trên port đã khởi tạo trước đấy
 //nạp các thông tin khai báo ở trên rồi chạy (ví dụ như nạp routes)
-app.listen(port, () => {
+app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
