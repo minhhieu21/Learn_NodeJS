0f26a621c8dbdce6e195fe15f5d124c4cee376b1
minhhieu2104 <hieutmpd05172@fpt.edu.vn>
2023-10-21 21:41:45 +0700

    design navbar

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,22 +1,24 @@
 const connection = require('../config/database');
 
 const getHomepage = (req, res) => {
-    //process data
-    //call model
-    let users = [];
+    // //process data
+    // //call model
+    // let users = [];
 
-    connection.query(
-        'SELECT * FROM Users u',
-        function(err, results, fields) {
-            users = results;
-            console.log(">>>>result home page = ", results); // results contains rows returned by server
-            // console.log(">>>>fields = ", fields); // fields contains extra meta data about results, if available
+    // connection.query(
+    //     'SELECT * FROM Users u',
+    //     function(err, results, fields) {
+    //         users = results;
+    //         console.log(">>>>result home page = ", results); // results contains rows returned by server
+    //         // console.log(">>>>fields = ", fields); // fields contains extra meta data about results, if available
 
 
-            // console.log(">>>>check users = ", users); 
-            res.send(JSON.stringify(users))
-        }
-    );
+    //         // console.log(">>>>check users = ", users); 
+    //         res.send(JSON.stringify(users))
+    //     }
+    // );
+
+    return res.render('home.ejs')
 
 }
 const getTest = (req, res) => {
