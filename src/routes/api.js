c12f5447942bbdd70a554c8413822a2b356bd2a3
minhhieu2104 <hieutmpd05172@fpt.edu.vn>
2024-02-14 20:58:41 +0700

    create customer with api

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const routerAPI = express.Router()
 const { getAllUsersAPI, postCreateUserAPI, putUpdateUserAPI, deleteUserAPI, postUploadSingleFileAPI, postUploadMultipleFilesAPI } = require('../controllers/apiController')
+const { postCreateCustomerAPI } = require('../controllers/customerController')
+    //khai báo routes
+    //req (request), res(response) là 2 object trong môi trường Node.js
+    //router.Method('/route',handler)
 
-//khai báo routes
-//req (request), res(response) là 2 object trong môi trường Node.js
-//router.Method('/route',handler)
 
+//Users
 routerAPI.get('/users', getAllUsersAPI);
 routerAPI.post('/users', postCreateUserAPI);
 routerAPI.put('/users', putUpdateUserAPI);
 routerAPI.delete('/users', deleteUserAPI);
 
+//Customers
+routerAPI.post('/customers', postCreateCustomerAPI);
+
+//File
 routerAPI.post('/file', postUploadSingleFileAPI);
 routerAPI.post('/files', postUploadMultipleFilesAPI);
 module.exports = routerAPI; //export default
